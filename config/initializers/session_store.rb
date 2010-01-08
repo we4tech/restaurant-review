cb0133dea630784b1722bf85084c8f3f5f8ee594
nhm tanveeer hossain khan <hasan@hasan.local>
2010-01-08 11:10:36 +0600

    - changed session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_restaurant-review_session',
+  :key         => '_welltreat_us',
   :secret      => '3589d4281cd34f5d0d0085f7058bcf660d2c5a326ac1d1dd22a5cef651f81b59b4519834a8f05fe663c8b9921a7a47944a1772a9d3e340806bc4b7e3e9350b8f'
 }
 
