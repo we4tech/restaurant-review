21b219d14c775cd8020b3bd3a83e38d16b084bfb
nhm tanveeer hossain khan <hasan@hasan.local>
2010-01-15 23:23:06 +0600

    - Fixed www related problem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,11 @@
 module ApplicationHelper
 
   def detect_topic_or_forward_to_default_one
-    topic_hint = request.subdomains || []
-    if topic_hint.empty?
-      @topic = Topic.default
+    topic_hint = (request.subdomains || []).join("_")
+    if topic_hint.empty? || topic_hint == 'www'
+      topic = Topic.default
+      redirect_to root_url(:subdomain => topic.name)
+      return
     else
       @topic = Topic.find_by_name(topic_hint)
     end
