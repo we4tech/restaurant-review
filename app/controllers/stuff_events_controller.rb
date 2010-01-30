8d5b753131a66e5f27a6dcb427cddeb47f7a5c4a
nhm tanveeer hossain khan <hasan@hasan.local>
2010-01-31 00:59:02 +0600

    - [DONE] 71. Display tag - [DONE] On welltreat.us it should redirect to the default topic, topic name "_" should be translated as "." - 62. [DONE] make generic url for edit profile - 60. [DONE] Stop back button while creating new restaurant - 78. [DONE] PUblic time line

--- app/controllers/stuff_events_controller.rb
+++ app/controllers/stuff_events_controller.rb
@@ -1,20 +1,30 @@
 class StuffEventsController < ApplicationController
 
-  before_filter :login_required
+  before_filter :login_required, :except => [:show]
   before_filter :log_new_feature_visiting_status
   after_filter  :log_last_visiting_time
 
   def show
     @site_title = 'Activities from others'
-    @subscribed_restaurants = current_user.subscribed_restaurants.find(:all, :group => 'restaurants.id')
-    @user_log = current_user.user_logs.by_topic(@topic.id).first
+    @subscribed_restaurants = []
+    @user_log = nil
+    conditions = nil
+
+    if current_user
+      @subscribed_restaurants = current_user.subscribed_restaurants.
+          find(:all, :group => 'restaurants.id')
+      @user_log = current_user.user_logs.by_topic(@topic.id).first
+      conditions = [
+          'restaurant_id IN (?) AND user_id != ?',
+          @subscribed_restaurants.collect{|r| r.id},
+          current_user.id]
+    else
+      conditions = []
+    end
 
     @stuff_events = StuffEvent.paginate(
         :include => [:restaurant, :review, :review_comment],
-        :conditions => [
-            'restaurant_id IN (?) AND user_id != ?',
-            @subscribed_restaurants.collect{|r| r.id},
-            current_user.id],
+        :conditions => conditions,
         :order => 'created_at DESC',
         :page => params[:page])
 
