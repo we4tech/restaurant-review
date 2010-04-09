e8e01aad1f54fa4cb0ee026e89294fa894b22006
nhm tanveeer hossain khan <hasan@hasan.local>
2010-04-09 10:18:07 +0600

    Fixed action controller related bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,10 @@ ActionMailer::Base.smtp_settings = {
 
 config.action_mailer.default_url_options = {:host => 'restaurant.welltreat.us'}
 
-ActionController::Base.session = {
-  :key         => '_welltreatus_prod',
-  :secret      => 'f11cf195514c9f70d208c7860c97b77b2f2fa19cc1b7291d00e26c89530a75077146bc9f0ec4076af5c5fe9b74ec1a9a3de6158a1840712d801103022417cf67',
-  :domain      => '.welltreat.us'
-}
+if defined?(ActionController)
+  ActionController::Base.session = {
+    :key         => '_welltreatus_prod',
+    :secret      => 'f11cf195514c9f70d208c7860c97b77b2f2fa19cc1b7291d00e26c89530a75077146bc9f0ec4076af5c5fe9b74ec1a9a3de6158a1840712d801103022417cf67',
+    :domain      => '.welltreat.us'
+  }
+end
