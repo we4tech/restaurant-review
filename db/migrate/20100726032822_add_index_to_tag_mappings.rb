c0731047fb495ef415cafbf2da3e5131c7df7227
nhm tanveeer hossain khan <hasan@hasan.local>
2010-07-26 10:41:27 +0700

    Release - 0.12 - [DONE] 131. Search tags - [DONE] 139. Add location tag - [DONE] 106. Geo tagging for restaurant page

--- db/migrate/20100726032822_add_index_to_tag_mappings.rb
+++ db/migrate/20100726032822_add_index_to_tag_mappings.rb
@@ -1,13 +1,11 @@
 class AddIndexToTagMappings < ActiveRecord::Migration
   def self.up
-    remove_index :tag_mappings, [:tag_id]
     remove_index :tag_mappings, [:tag_id, :topic_id]
     remove_index :tag_mappings, [:restaurant_id]
     add_index :tag_mappings, [:tag_id, :restaurant_id, :topic_id]
   end
 
   def self.down
-    add_index :tag_mappings, [:tag_id]
     add_index :tag_mappings, [:tag_id, :topic_id]
     add_index :tag_mappings, [:restaurant_id]
     remove_index :tag_mappings, [:tag_id, :restaurant_id, :topic_id]
