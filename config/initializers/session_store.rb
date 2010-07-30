91c48868cc9a4d8c6a38243e997c13c94069f378
nhm tanveeer hossain khan <hasan@hasan.local>
2010-07-31 01:15:02 +0700

    Release - 0.12 - [DONE] 148. Add Meta description for each restaurants - Moved recommend from restautants controller to home controller - [DONE] 147. Add FB like button on review - [DONE] 141. Support facebook profile Tab - [DONE] 138. FB page on received approved treat - [DONE] 149. fix fb like button width - [DONE] 150. remove fb like button from show restaurant page - [DONE] 152. Fixed broken tag details page url

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_welltreat_us',
+  :key         => '_welltreat_us_prod',
   :secret      => '3589d4281cd767676773989898d08989085fbc60d2c5a326ac1d1dd22a5cef651f81b59b4519834a8f05fe663c8b9921a7a47944a1772a9d3e340806bc4b7e3e9350b8f'
 }
 
