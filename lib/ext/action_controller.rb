8d82af78a00bb3f45fcf5481795bfeb7da91de88
nhm tanveeer hossain khan <hasan@hasan.local>
2010-08-04 01:15:28 +0700

    ---------------- Release - 0.14 ---------------- - Fixed broken image url

--- lib/ext/action_controller.rb
+++ lib/ext/action_controller.rb
@@ -9,10 +9,12 @@ ActionController::Base::class_eval do
     elsif options.is_a?(String)
       if options.match(/l=\w+/)
         options.gsub!(/l=(\w+)/, "l=#{I18n.locale.to_s}")
-      elsif !options.match(/\?/)
-        options << "?l=#{I18n.locale.to_s}"
-      elsif options.match(/\?/)
-        options << "&l=#{I18n.locale.to_s}"
+      elsif !options.match(/\.(jpg|gif|png|swf|jpeg|ani|ico)$/)
+        if !options.match(/\?/)
+          options << "?l=#{I18n.locale.to_s}"
+        elsif options.match(/\?/)
+          options << "&l=#{I18n.locale.to_s}"
+        end
       end
 
       __url_for(options)
--- lib/ext/action_view.rb
+++ lib/ext/action_view.rb
@@ -9,10 +9,12 @@ ActionView::Helpers::UrlHelper.module_eval do
     elsif options.is_a?(String)
       if options.match(/l=\w+/)
         options.gsub!(/l=(\w+)/, "l=#{I18n.locale.to_s}")
-      elsif !options.match(/\?/)
-        options << "?l=#{I18n.locale.to_s}"
-      elsif options.match(/\?/)
-        options << "&l=#{I18n.locale.to_s}"
+      elsif !options.match(/\.(jpg|gif|png|swf|jpeg|ani|ico)$/)
+        if !options.match(/\?/)
+          options << "?l=#{I18n.locale.to_s}"
+        elsif options.match(/\?/)
+          options << "&l=#{I18n.locale.to_s}"
+        end
       end
 
       __url_for(options)
