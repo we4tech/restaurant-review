2f736c249c8b1bdace91313b2acdc971513aaa9d
nhm tanveeer hossain khan <hasan@hasan.local>
2010-08-16 16:14:28 +0700

    - Fixed contributed image relation related problem

--- app/models/image.rb
+++ app/models/image.rb
@@ -43,6 +43,19 @@ class Image < ActiveRecord::Base
       end
     end
 
+    applicable_keys = ContributedImage.new.attributes.keys.collect{|key| key if key.to_s.match(/_id/)}
+    applicable_keys.delete('user_id')
+    applicable_keys.delete('image_id')
+    applicable_keys.delete('topic_id')
+    applicable_keys.compact!
+    
+    self.contributed_images.each do |contb_image|
+      related_object = detect_related_object(applicable_keys, contb_image)
+      if related_object
+        return related_object
+      end
+    end
+
     return nil
   end
 
