da40e671887b3ea88cd2cae04d0e71cea3729e1a
nhm tanveeer hossain khan <hasan@hasan.local>
2010-09-21 18:39:50 +0700

    - Fixed IE related issues - [DONE] 183. FIX - page = nil related problem for WIllPagination - Added label for new field - phone - Fixed email notification related problem

--- app/helpers/premium_helper.rb
+++ app/helpers/premium_helper.rb
@@ -9,8 +9,13 @@ module PremiumHelper
   end
 
   def premium?
+    session = session || {}
     return true if @premium
     return true if session[:premium]
     false
+  end
+
+  def pt_image_tag(file_name, options = {})
+    image_tag("templates/#{@premium_template.template}/#{file_name}", options)
   end
 end
