202268fd2d6f9abf1d4ca4c84b6dcd1a02ccad8b
nhm tanveeer hossain khan <hasan@hasan.local>
2010-10-25 00:13:17 +0700

    - [DONE] 196. Added support for old recommend url - [DONE] 197. Ajaxify search filtering box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => 'new'
+      render_view('users/new')
     end
   end
 
