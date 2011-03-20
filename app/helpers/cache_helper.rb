3126200fe3a295e8c8c992686b9fe9f618adbddf
nhm tanveeer hossain khan <hasan@hasan.local>
2011-03-20 11:20:30 +0600

    - Building new template engine - [DONE] 323. Fix problem with Mobile version restaurant details page (shows not supported) - [DONE] 323. Fix problem with Mobile version restaurant details page (shows not supported) - [DONE] 323. Integrate Google Map V3 - [DONE] 325. Integrated auto current location detection - [DONE] 326. Bring furniture reviewer community - Updated icons and logos

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -22,11 +22,13 @@ module CacheHelper
   end
 
   class Util
-    def self.expire_caches(path, match)
-      files = Dir.glob(File.join(RAILS_ROOT, 'tmp', 'cache', 'views', '*', path, '*'))
-      files.each do |file|
-        if file.match(/#{match}/)
-          File.delete(file)
+    class << self
+      def expire_caches(path, match)
+        files = Dir.glob(File.join(RAILS_ROOT, 'tmp', 'cache', 'views', '*', path, '*'))
+        files.each do |file|
+          if file.match(/#{match}/)
+            FileUtils.rm_rf(file)
+          end
         end
       end
     end
