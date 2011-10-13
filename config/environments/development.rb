47e59fd7309de410cae7db06724c07a92fe24355
we4tech <hasan83bd@gmail.com>
2011-10-13 18:40:20 +0600

    - Fixed broken file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_controller.asset_host = "http://ajax%d.hadok.com"
 
+config.action_controller.cache_store = :file_store, RAILS_ROOT+"/tmp/cache/"
+
+
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :sendmail
